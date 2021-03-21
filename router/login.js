84743e56f27a2e0a04d56076711d5482436b50d5
DurgeshTiwari <durgesh.tiw98@gmail.com>
2021-03-22 04:33:34 +0530

    internship_assignment_node_project

---
+++ router/login.js
@@ -0,0 +1,101 @@
+// const mongoose = require('mongoose');
+const config = require('config');
+const jwt=require('jsonwebtoken');
+const Joi=require('Joi');
+const _=require('lodash');
+const Bcryptjs=require('bcryptjs');
+const auth=require('../middleware/authentication');
+const validate=require('../middleware/validateUserBaseOnToken');
+const {Customer}=require('../api/userSchema');
+const express = require('express');
+// const { values } = require('lodash');
+const router=express.Router();
+
+
+//without varification or token you Can't get whole info about User
+
+router.get('/:_id',auth,validate,async(req,res)=>{
+    const userInfo=await Customer.findById(req.params._id);
+    return res.send({
+    "UserINfo":userInfo,
+    "UnhashPasswod":req.user.password
+    });
+});
+
+//Admin as User try to log in-----------------------------------------------------------------
+
+router.post('/',auth,validate,async(req, res)=>{
+const{error}=validateUserasAuth(req.user);
+if(error)return res.status(400).send('Not enter the valid email/Id');
+const userdb=await Customer.findOne({_id:req.user._id});
+// console.log(userdb)
+if(!userdb)return res.status(400).send('Invalid_User');
+const auth=await Bcryptjs.compare(req.user.password,userdb.password);
+if(!auth)return res.status(400).send('email or password is worng ');
+//genrating token and put the jwt.sign in diifrent module  
+//the arrow fuction does not have t6his keyword .
+const values=`--------------You log in successfully As Admin 
+  Email_ID:${req.user.email} and Password:${req.user.password}---------------`;
+const token=jwt.sign({_id:userdb._id,isAdmin:userdb.isAdmin},config.get('internshiprProjectPrivateKey'));//use to genrate json web token
+userdb.adminTokenAfterLogin=token;
+
+const result=await userdb.save();
+return res.send([{"Token_after_Login_Successfully":result,"Login_Information_of_User":values}]);
+// return res.send(_.pick(['token']));
+});
+
+//Normal user without token who is not admin-------------------------------------------------
+
+router.post('/normal',async(req, res)=>{
+    const{error}=validateUserasAuth(req.body);
+    if(error)return res.status(400).send('Not enter the valid email/Id');
+    const userdb=await Customer.findOne({_id:req.body._id});
+    // console.log(userdb)
+    if(!userdb)return res.status(400).send('Invalid_User');
+    const auth=await Bcryptjs.compare(req.body.password,userdb.password);
+    if(!auth)return res.status(400).send('email or password is worng ');
+    //genrating token and put the jwt.sign in diifrent module  
+    //the arrow fuction does not have t6his keyword .
+    const values=`--------------You log in successfully you privilege is limited for 
+      Email_ID:${req.body.email} and Password:${req.body.password}---------------`;
+    const token=jwt.sign({_id:userdb._id,isAdmin:userdb.isAdmin},config.get('internshiprProjectPrivateKey'));//use to genrate json web token
+    userdb.adminTokenAfterLogin=token;
+    
+    const result=await userdb.save();
+    return res.send([{"Token_after_Login_Successfully":result,"Login_Information_of_User":values}]);
+    // return res.send(_.pick(['token']));
+    });
+
+// forget password by normal user-------------------------------------------------------------------
+
+router.post('/forgetPassword',async(req, res)=>{
+    const{error}=validateUserasAuth(req.body);
+    if(error)return res.status(400).send('Not enter the valid emailId or password type');
+    const userdb=await Customer.findOne({email:req.body.email});
+    if(!userdb)return res.status(400).send('Invalid_User');
+    const salt=await Bcryptjs.genSalt(10);
+    userdb.password=await Bcryptjs.hash( req.body.password,salt);
+    
+    //genrating token and put the jwt.sign in diifrent module  
+    //the arrow fuction does not have t6his keyword .
+    const values=`--------------You Update you password successfuly 
+      Email_ID:${req.body.email} and Password:${req.body.password}---------------`;
+    
+    const result=await userdb.save();
+    return res.send({"Rsult":values,"updateInfo":_.pick(result,['_id','password'])});
+    // return res.send(_.pick(['token']));
+    });
+
+
+
+
+function validateUserasAuth({email,password}){
+    const schema=Joi.object({
+        email:Joi.string().max(255).required().email(),
+        password:Joi.string().max(255).required()
+    });
+    return schema.validate({email,password});
+}
+//get request data as admin that include the password of user as well for as Admin
+
+module.exports=router;
---
+++ router/registrationRouter.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const Bcryptjs=require('bcryptjs');
+const {Customer,validateUser}=require('../api/userSchema');
+const auth=require('../middleware/authentication');
+const validate=require('../middleware/validateUserBaseOnToken');
+const _=require('lodash');
+const jwt=require('jsonwebtoken');
+const config=require('config');
+const router=express.Router();
+
+router.get('/:id',async(req,res)=>{
+    const customer=await Customer.findById(req.params.id).select(['-password','-securans','-isAdmin','-adminTokenAfterLogin','-SecurityAns']);
+     return res.send(customer);
+});
+router.post('/',async(req,res)=>{
+    const {error}=validateUser(req.body);
+    if(error) return res.status(400).send('Value enter by you may be not valid try again......');
+    let userInfo=await Customer.findOne({email:req.body.email});
+    if(userInfo)return res.status(400).send('user is register with this email is alrady exist');
+    const customer=new Customer({
+        name:req.body.name,
+        email:req.body.email,
+        password:req.body.password,
+        SecurityQuestion:req.body.securque,
+        SecurityAns:req.body.securans,
+    });
+    const passwordSend=customer.password;
+    const salt=await Bcryptjs.genSalt(10);
+    customer.password=await Bcryptjs.hash( customer.password,salt);
+    customer.SecurityQuestion=await Bcryptjs.hash( customer.SecurityQuestion,salt);
+    customer.SecurityAns=await Bcryptjs.hash( customer.SecurityAns,salt);
+    const token=jwt.sign({_id:customer._id,email:customer.email,password:passwordSend},config.get('internshiprProjectPrivateKey'));
+    // customer.Token=token;
+    const result=await customer.save();
+    return res.header('x-auth-validation',token).send(_.pick(result,['_id','name','email','password']));
+    // res.send(result);
+})
+router.get('/:id',async(req,res)=>{
+    const customer= await Customer.findById(req.params.id);
+    if(!customer)return res.status(404).send("The Customer with this id is not exists ");
+    return res.send(customer)
+
+});
+//Update Information only admin can modify it--here admin try to revoke privilege 
+
+router.put('/',[auth,validate],async(req,res)=>{
+    const customer=await Customer.findByIdAndUpdate(req.body.id,{
+        isAdmin:req.body.isAdmin
+    },{new:true});
+    const customerInformationUpdated=await customer.save();
+    return res.send({
+        "User":customerInformationUpdated,
+        "Info":`For user with id:${req.params.id} privilege change Successfully Now his access limited`
+    });
+})
+
+//delete is also  done by user only-------------------------------
+
+router.delete('/:id',[auth,validate],async(req,res)=>{
+    const customer=await Customer.findByIdAndRemove(req.params.id);
+    return res.send({
+        "User":customer,
+        "Info":`The user with id:${req.params.id} is Deleted Successfully`
+    });
+})
+
+
+
+
+module.exports=router;
