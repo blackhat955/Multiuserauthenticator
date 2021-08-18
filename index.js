39d9482ac6579a8066318e05c211f3247050966e
Durgesh Tiwari <61361185+blackhat955@users.noreply.github.com>
2021-08-19 03:08:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -29,6 +29,8 @@ app.get('/',async(req,res)=>{
 {"/api/register/normal":"register as normal user"},
 {"put request at .api/register/":"requset to pass req.body.isAdmin"},
 {"delete /api/register/id":"pass id as parameter"},
+{"see one demo collection"},
+{https://multiuserauthencator.herokuapp.com/api/register},
 {"the screenshot of testing of api are attach my github respository":"pls have a look"},
 {githubLink}])
 });
